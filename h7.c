c2685c52464fac3e4dc1170340b4da21ec65acb6
zheng-zi <56283334+zheng-zi@users.noreply.github.com>
2019-12-10 00:16:29 +0800

    Add files via upload

---
+++ h7.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(void)
+{
+    int x;
+    draw(x);
+}
+
+int draw(int x)
+{
+int i,b,c,d,e;
+
+
+
+  for(i=1;i<=1;i++)
+    {
+        printf("w");
+        for(b=1;b<=1;b++){
+            
+                printf("   ");
+                printf("w");
+                printf("\n");}
+
+                for(c=1;c<=1;c++)
+                    
+                       { printf(" ");}
+                    for(d=1;d<=1;d++)
+                       {printf(" ");
+                       printf("w");
+                       }
+        }
+    }
